121c1d8712d9f69e8d70331ba0b885314a4849d8
春雨 <chunyu@192.168.36.150>
2018-02-02 20:01:22 +0800

    提交

---
+++ src/components/mycom.vue
@@ -0,0 +1,18 @@
+<template>
+ <div id='mycom'>
+   <h1>
+     hi my name is {{myname}}
+   </h1>
+
+ </div>
+</template>
+<script>
+export default {
+    name: 'mycom',
+    data () {
+        return {
+            myname: 'fanyy'
+        }
+    }
+}
+</script>

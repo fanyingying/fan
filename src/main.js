068a72e4c855a3aa811883c882870a78b268fdc4
fanyingying <1178223444@qq.com>
2018-03-08 20:19:54 +0800

    v0.0.1

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 Vue.use(ElementUI)
 
+
 // import BgaBackTop from 'bga-back-top-vue'
 // Vue.use(BgaBackTop)
 import marked from 'marked'
@@ -28,6 +29,8 @@ import { gitHubApi, isGetUserInfo, queryParse, queryStringify } from './utils'
 
 
 Vue.config.productionTip = false
+
+
 
 Vue.prototype.$http = axios
 //Vue.prototype.$highlight = highlight
@@ -53,10 +56,54 @@ Vue.prototype.$queryStringify = queryStringify
 Vue.prototype.$marked = marked
 
 /* eslint-disable no-new */
-new Vue({
+const vm = new Vue({
   el: '#app',
   router,
   store,
   components: { App },
   template: '<App/>'
+})
+let loadingInstance
+
+// request拦截器
+axios.interceptors.request.use((config) => {
+  let isGetUserInfo = vm.$isGetUserInfo(vm, config)
+  if (!isGetUserInfo) {
+    loadingInstance = ElementUI.Loading.service({
+      text: '拼命加载中...'
+    })
+  }
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+// response拦截器
+axios.interceptors.response.use((response) => {
+  let isGetUserInfo = vm.$isGetUserInfo(vm, response.config)
+  if (isGetUserInfo) {
+    return response
+  } else {
+    setTimeout(() => {
+      loadingInstance.close()
+    }, 500)
+    return response
+  }
+}, (error) => {
+  let isGetUserInfo = vm.$isGetUserInfo(vm, error.config)
+  if (!isGetUserInfo) {
+    loadingInstance.close()
+
+    if (error.response) {
+      if (error.response.status === 401) {
+        vm.$warningMessage('登录信息已过期，请重新登录!')
+      } else if (error.response.status === 403) {
+        vm.$warningMessage('您操作的太频繁，请稍后再试!')
+      } else if (error.response.statusText) {
+        vm.$errorMessage(error.response.status + ' ' + error.response.statusText)
+      }
+    }
+  }
+
+  return Promise.reject(error)
 })
